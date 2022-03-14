ba0f818f85f4b78613fde4d926afdac533cec731
Melonvin <1176891456@qq.com>
2022-03-14 11:55:27 +0800

    update

---
+++ 02-数据类型/types.js
@@ -0,0 +1,21 @@
+// undefined 和 null
+var a = undefined;
+var b = null;
+// number
+var decimal = 1;
+var hex = 0xff;
+var binary = 5;
+var octal = 511;
+// string
+var str1 = "Hello TypeScript";
+var myName = "TypeScript";
+var str2 = "Hello " + myName;
+var age = "My age is " + (decimal + 1);
+// boolean
+var isDone = true;
+var isTrue = false;
+// bigint
+// 注意需要在整数字面量后面加n来定义一个 BigInt
+var big = 9007199254740991n;
+// symbol
+var symbol1 = Symbol("key");
---
+++ 02-数据类型/types.ts
@@ -0,0 +1,26 @@
+// undefined 和 null
+let a: undefined = undefined;
+let b: null = null;
+
+// number
+let decimal: number = 1;
+let hex: number = 0xff;
+let binary: number = 0b101;
+let octal: number = 0o777;
+
+// string
+let str1: string = "Hello TypeScript";
+let myName: string = `TypeScript`;
+let str2: string = `Hello ${myName}`;
+let age: string = `My age is ${decimal + 1}`;
+
+// boolean
+let isDone: boolean = true;
+let isTrue: boolean = false;
+
+// bigint
+// 注意需要在整数字面量后面加n来定义一个 BigInt
+let big: bigint = 9007199254740991n;
+
+// symbol
+let symbol1: symbol = Symbol("key");
